f982df7ba1be4c28d99afd1068a9c29bc428ad52
Shalin <shalin_jirawla@hotmail.com>
2025-09-03 10:34:17 +0530

    solve service proxi conflict

--- 10.2.0/angular/src/shared/service-proxies/service-proxies.ts
+++ 10.2.0/angular/src/shared/service-proxies/service-proxies.ts
@@ -13671,6 +13671,58 @@ export class PharmacistPrescriptionsServiceProxy {
     }
 
     /**
+     * @param body (optional) 
+     * @return OK
+     */
+    createPharmacistPrescriptionsWithItem(body: CreateUpdatePharmacistPrescriptionsDto | undefined): Observable<void> {
+        let url_ = this.baseUrl + "/api/services/app/PharmacistPrescriptions/CreatePharmacistPrescriptionsWithItem";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(body);
+
+        let options_ : any = {
+            body: content_,
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Content-Type": "application/json",
+            })
+        };
+
+        return this.http.request("post", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processCreatePharmacistPrescriptionsWithItem(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processCreatePharmacistPrescriptionsWithItem(response_ as any);
+                } catch (e) {
+                    return _observableThrow(e) as any as Observable<void>;
+                }
+            } else
+                return _observableThrow(response_) as any as Observable<void>;
+        }));
+    }
+
+    protected processCreatePharmacistPrescriptionsWithItem(response: HttpResponseBase): Observable<void> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (response as any).error instanceof Blob ? (response as any).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            return _observableOf(null as any);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf(null as any);
+    }
+
+    /**
      * @param id (optional) 
      * @return OK
      */
@@ -34642,8 +34694,6 @@ export class Prescription implements IPrescription {
                 for (let item of _data["ipdChargeEntries"])
                     this.ipdChargeEntries.push(IpdChargeEntry.fromJS(item));
             }
-        }
-    }
             this.pharmacistPrescriptions = _data["pharmacistPrescriptions"] ? PharmacistPrescriptions.fromJS(_data["pharmacistPrescriptions"]) : <any>undefined;
             this.consultation_Requests = _data["consultation_Requests"] ? ConsultationRequests.fromJS(_data["consultation_Requests"]) : <any>undefined;
         }
